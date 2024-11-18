4bff8a68ada3ef54e4415c845f621a46447f20a1
wmxpy <wm@mengw.io>
2024-11-18 16:21:25 -0600

    add RawDatabase component and update dependencies

---
+++ src/database/raw-database.tsx
@@ -0,0 +1,18 @@
+/**
+ * @author WMXPY
+ * @namespace Database
+ * @description Raw Database
+ */
+
+import { IImbricateDatabase } from "@imbricate/core";
+import React, { FC } from "react";
+
+export type RawDatabaseProps = {
+
+    databases: IImbricateDatabase[];
+};
+
+export const RawDatabase: FC = () => {
+
+    return <div></div>;
+};
