05c084904bbfecee01850e9b4dd7af9ecabda801
wmxpy <wm@mengw.io>
2024-12-02 11:49:59 -0600

    feat: refactor SearchView component; improve layout and styling for search input and results

--- src/search/search-view.tsx
+++ src/search/search-view.tsx
@@ -38,41 +38,43 @@ export const SearchView: FC = () => {
         setLoading(false);
     };
 
-    return (<div>
-        <div className="mt-5 mb-5">
-            <div
-                className="flex gap-2"
+    return (<div className="h-full flex flex-col">
+        <div
+            className="flex gap-2 pt-4 pr-2"
+        >
+            <Input
+                autoFocus
+                className="flex-1"
+                variant="bordered"
+                color="primary"
+                label="keyword"
+                value={keyword}
+                onChange={(event) => {
+                    setKeyword(event.target.value);
+                }}
+                onKeyDown={(event: any) => {
+                    if (event.key === "Enter") {
+                        performSearch();
+                    }
+                }}
+            />
+            <Button
+                className="w-[56px] h-[56px]"
+                variant="flat"
+                color="primary"
+                isIconOnly
+                onClick={performSearch}
+                isLoading={loading}
             >
-                <Input
-                    autoFocus
-                    className="flex-1"
-                    variant="bordered"
-                    color="primary"
-                    label="keyword"
-                    value={keyword}
-                    onChange={(event) => {
-                        setKeyword(event.target.value);
-                    }}
-                    onKeyDown={(event: any) => {
-                        if (event.key === "Enter") {
-                            performSearch();
-                        }
-                    }}
-                />
-                <Button
-                    className="w-[56px] h-[56px]"
-                    variant="flat"
-                    color="primary"
-                    isIconOnly
-                    onClick={performSearch}
-                    isLoading={loading}
-                >
-                    <FaSearch />
-                </Button>
-            </div>
+                <FaSearch />
+            </Button>
         </div>
-        <SearchResult
-            searchResults={searchResults}
-        />
+        <div
+            className="flex-1 overflow-auto min-w-0 min-h-0 my-2 pr-2"
+        >
+            <SearchResult
+                searchResults={searchResults}
+            />
+        </div>
     </div>);
 };
